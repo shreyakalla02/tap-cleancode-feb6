9e414e5935ed8635041880e55191a1a306282a34
shreyakalla02 <150719217+shreyakalla02@users.noreply.github.com>
2025-02-06 19:14:46 +0000

    format with pep8

--- a_app.py
+++ a_app.py
@@ -11,27 +11,28 @@ list_sample = [1, 2, 3, "Four", 5, 6]
 print("     Item 1:", list_sample[0])
 print("     Item 4:", list_sample[3])
 print("  Item Last:", list_sample[-1])
-print("Slice items:", list_sample[2:len(list_sample)], f" array length: {len(list_sample)}")
+print("Slice items:", list_sample[2:len(
+    list_sample)], f" array length: {len(list_sample)}")
 # Loop through Lists
 print(" ---- Listing Items in List ----")
 for item in list_sample:
-  print(item)
+    print(item)
 print(" ---- Listing Items using range() ----")
 # for using range(start,stop,step) / range(stop)
 for index in range(len(list_sample)):
-  print(index, ":", list_sample[index])
+    print(index, ":", list_sample[index])
 print(hr)
 
 email = "j@em2.com"
 
 # Dictionary : { key_1: value_1, key_2: value_2, ..... key_n: value_n }
 dictionary_sample = {
-  "id" : 123,
-  "name": "John",
-  "email" : [
-      "j@em.com",
-      email
-  ]
+    "id": 123,
+    "name": "John",
+    "email": [
+        "j@em.com",
+        email
+    ]
 }
 
 # Access dictionaries individually
@@ -40,10 +41,9 @@ print("     Id:", dictionary_sample["id"])
 print("   Name:", dictionary_sample["name"])
 print(" Emails:", dictionary_sample["email"])
 print("Email 1:", dictionary_sample["email"][0])
-print("Email 2:",dictionary_sample["email"][1])
+print("Email 2:", dictionary_sample["email"][1])
 print(hr)
 # Loop through dictionaries print key : value - element type
 for key in dictionary_sample:
-  print(key, ":", dictionary_sample[key])
+    print(key, ":", dictionary_sample[key])
 print("-"*20)
-
